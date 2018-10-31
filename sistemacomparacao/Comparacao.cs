4dcf3946d9dbf319964b4fba142b575284aedbf5
andreivercosa <aluno@MacBook-Pro-de-Aluno.local>
2018-10-31 19:14:55 -0400

    comparar dois numeros

---
+++ sistemacomparacao/Comparacao.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace sistemacomparacao
+{
+    public class Comparacao
+    {
+        public int numero1Tela{
+            set;get;
+        }
+
+        public int numero2Tela
+        {
+            set; get;
+        }
+
+        public string comparar(){
+            if(numero1Tela > numero2Tela){
+                return "Primeiro numero maior que o segundo";
+            }else if(numero2Tela > numero1Tela){
+                return "Segundo numero maior que o primeiro";
+            }else{
+                return "Os numero sao iguais";
+            }
+        }
+
+    }
+}
---
+++ sistemacomparacao/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace sistemacomparacao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int test = 1;
+            do
+            {
+
+                Comparacao comparacao = new Comparacao();
+
+                Console.WriteLine("Informe o Primeiro Numero: ");
+                comparacao.numero1Tela = Int32.Parse(Console.ReadLine());
+
+                Console.WriteLine("Informe o Segundo Numero: ");
+                comparacao.numero2Tela = Int32.Parse(Console.ReadLine());
+
+                Console.WriteLine(comparacao.comparar());
+
+                Console.WriteLine("\n\nDeseja continuar: \n1 - Sim\n2 - Nao");
+                test = Int32.Parse(Console.ReadLine());
+
+            } while (test == 1);
+        }
+    }
+}
